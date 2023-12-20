bc10612d6585465f38192540f4610c621c8cbe10
jorge06 <jorgevn06@gmail.com>
2023-12-20 19:36:38 -0300

    error navigation

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/Pantallas/Home';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
+const Stack = createNativeStackNavigator();
 
+const Inicio = (navigation) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Inicio Screen</Text>
+      <Button title="Home" onPress={()=> navigation.navigate ("Listado")}/>
+    </View>
+  );
+}
+
+const Listado = (navigation) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Listado Screen</Text>
+    </View>
+  );
+}
 const App = () => {
 
   return (
-    
-    <View style={styles.container}>
-    <Home/>
-    </View>
+    <>
+      <StatusBar/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Inicio" component={Inicio} />
+          <Stack.Screen name="Listado" component={Listado} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </>
   );
 }
 
